6631d24dcb64d889162575f7786eeede7ebcb9f4
Paul Molloy <molloyp1@tcd.ie>
2016-12-04 10:54:11 +0000

    Implement sending string using datagramPackets and sockets to another Router. Add sendMessage(String message, int port)

--- src/cs/tcd/ie/Router.java
+++ src/cs/tcd/ie/Router.java
@@ -1,7 +1,13 @@
 package cs.tcd.ie;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
 import java.util.ArrayList;
+
+import tcdIO.Terminal;
 
 
 public class Router extends Node{
@@ -11,16 +17,28 @@ public class Router extends Node{
 	private int x, y;
 	private String routerName;
 	private int port;
+	static final int DEFAULT_SRC_PORT = 50000;
+	static final int DEFAULT_DST_PORT = 50001;
+	static final String DEFAULT_DST_NODE = "localhost";	
+	
+	static InetSocketAddress dstAddress=new InetSocketAddress(DEFAULT_DST_NODE, DEFAULT_DST_PORT);
 	
 	
 
 
-	public Router(int x, int y, ArrayList<User> users, String routerName, int port) {
+	public Router(int x, int y, ArrayList<User> users, String routerName, int port) { 	//possibly add adjacent ports to constuctor
 		listOfRouters  = new ArrayList<Router>();
 		table = new RoutingTable(this, users);
 		this.setX(x);
 		this.setY(y);
 		this.setRouterName(routerName);
+		try {
+			socket= new DatagramSocket(this.getPort());
+			listener.go();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
@@ -40,13 +58,37 @@ public class Router extends Node{
 	public synchronized void onReceipt(DatagramPacket packet) {
 		table.updateRoutingTable(new Message(packet.getData().toString()));
 	}
+
 	
 	/*
 	 * Sends normal messages from router A to B.
 	 */
 	@Override
 	public synchronized void sendMessage(String message) {
+		DatagramPacket packet= null;
+		dstAddress=new InetSocketAddress(DEFAULT_DST_NODE, DEFAULT_DST_PORT);
+		packet= new DatagramPacket(message.getBytes(),message.getBytes().length, dstAddress);
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+	}
+	
+	@Override
+	public void sendMessage(String message, int dstPort) {
+		// TODO Auto-generated method stub
+		DatagramPacket packet= null;
+		dstAddress=new InetSocketAddress(DEFAULT_DST_NODE, dstPort);
+		packet= new DatagramPacket(message.getBytes(),message.getBytes().length, dstAddress);
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	/*
@@ -113,5 +155,9 @@ public class Router extends Node{
 	public void setRouterName(String routerName) {
 		this.routerName = routerName;
 	}
+
+
+
+
 
 }
