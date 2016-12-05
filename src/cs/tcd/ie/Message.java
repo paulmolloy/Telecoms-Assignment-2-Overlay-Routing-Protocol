45de9bfe7dd56fbe6cde281c7b516deb787255c4
paulmolloy <molloyp1@tcd.ie>
2016-12-05 22:51:06 +0000

    Add toDatagramPacket() method , Message(DatagramPacket packet) constructor

--- src/cs/tcd/ie/Message.java
+++ src/cs/tcd/ie/Message.java
@@ -1,5 +1,10 @@
 package cs.tcd.ie;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
 import java.util.ArrayList;
 
 public class Message {
@@ -10,6 +15,7 @@ public class Message {
 	 * each router. They possess getter and setter methods.
 	 */
 	
+	public static final int MESSAGE_CODE = 0;
 	private String message;
 	private String[] information;
 	
@@ -20,6 +26,78 @@ public class Message {
 	public Message(String message) {
 		this.message = message;
 		information = message.split(",");
+	}
+	
+	
+	/**Routingtable constructor that turns a DatagramPacket made from a RoutingTable into a RoutingTable
+	 * @param packet
+	 */
+	@SuppressWarnings("unchecked")
+	public Message (DatagramPacket packet) {
+
+		try {
+			
+
+			byte[] data;
+			ByteArrayInputStream bin;
+			ObjectInputStream oin;
+
+			data= packet.getData();  // use packet content as seed for stream
+			bin= new ByteArrayInputStream(data);
+			oin= new ObjectInputStream(bin);
+			
+			int packetType = oin.readInt();  // read type from beginning of packet
+
+			switch(packetType) {   // depending on type create content object 
+			case MESSAGE_CODE:
+
+				this.message = oin.readUTF();
+				break;
+			
+			default:
+				this.message  = null;
+
+				break;
+			}
+			oin.close();
+			bin.close();
+
+		}
+		catch(Exception e) {e.printStackTrace();}
+
+	}
+	
+
+	/**Converts a Message into a DatagramPacket to be sent
+	 * @return DatagramPacket
+	 */
+	public DatagramPacket toDatagramPacket() {
+		DatagramPacket packet= null;
+
+		try {
+			ByteArrayOutputStream bout;
+			ObjectOutputStream oout;
+			byte[] data;
+			bout= new ByteArrayOutputStream();
+			oout= new ObjectOutputStream(bout);
+			
+			oout.writeInt(MESSAGE_CODE);
+			oout.writeUTF(this.message);
+			
+			oout.flush();
+			data= bout.toByteArray(); // convert content to byte array
+
+			packet= new DatagramPacket(data, data.length); // create packet from byte array
+			oout.close();
+			bout.close();
+		}
+		catch(Exception e) {e.printStackTrace();}
+
+		return packet;
+	}
+	
+	public String toString(){
+		return this.message;
 	}
 	
 	/*
