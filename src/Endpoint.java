2bfe21407eb6a41e767d241017fb23898fb6ef2f
paulmolloy <molloyp1@tcd.ie>
2017-02-23 18:29:53 +0000

    Add file input code and rough endpoint class

---
+++ src/Endpoint.java
@@ -0,0 +1,28 @@
+
+public class Endpoint {
+	
+	private int l;
+	private int k;
+	
+	public int getl(){
+		return l;	
+	}
+	public int getk(){
+		return k;	
+	}
+	
+	public int setl(int l){
+		return l;
+	}
+	
+	public int setk(int l){
+		return k;
+	}
+	
+	
+	
+	
+	
+
+	//public class  
+}
--- src/HashCode.java
+++ src/HashCode.java
@@ -1,4 +1,56 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class HashCode {
 
+	
+	public static void main(String[] args){
+		
+		//Scanner in = new Scanner(new FileReader("filename.txt"));
+		getInput();
+
+	}
+	
+	private static void getInput(){
+		
+		try {
+			
+			Scanner in = new Scanner(new FileReader("kittens.in"));
+			int v, e,r,c,x;
+			
+			v= in.nextInt();
+			e= in.nextInt();
+			r= in.nextInt();
+			c= in.nextInt();
+			x= in.nextInt();
+			
+			System.out.println(v);
+			int[] videos = new int[v];
+			for(int i=0;i< v;i++){
+				videos[i] = in.nextInt();
+				
+			}
+			
+			Endpoint[] endpoints = new Endpoint[e]; 
+			for(int i=0;i< v;i++){
+				int l, k;
+
+				//endpoints[i].setl(in.nextInt());
+				//endpoints[i].setk(in.nextInt());
+
+				
+			}
+			
+//			while(in.hasNext()){
+//				
+//				System.out.println(v + x);
+//			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
